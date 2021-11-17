3857fe54208bbe6112af8bb6cc75f20cec991d51
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-11-17 16:51:50 +0100

    init project

---
+++ styles/UI_Elements.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Button = styled.div`
+  width: 150px;
+  height: 50px;
+  background-color: lightgray;
+  border-radius: 8px;
+  color: black;
+  box-shadow: 2px 2px black;
+  
+  &:hover {
+    box-shadow: none;
+  }
+`;
